430695a1ea6103c6ce6e09a7218badd91be81aa3
Lemishko Dmitry <81725793+AgroFermer@users.noreply.github.com>
2023-06-23 18:16:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ async def approve_request (chat_join: ChatJoinRequest, bot: Bot):
 
 
    await bot.send_message(chat_id=chat_join.from_user.id, text=msg, reply_markup=markup, disable_web_page_preview=True)
-   await chat_join.approve()
+ 
 
 async def start():
     logging.basicConfig(level=logging.DEBUG,
