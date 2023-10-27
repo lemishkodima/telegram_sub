dfc46fd70fc8722cf5a10f782055f16469893d3a
Lemishko Dmitry <81725793+AgroFermer@users.noreply.github.com>
2023-10-27 14:40:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ BOT_TOKEN = '6250154933:AAGiuVuTLqPcXM8bx1vJRM2BK2XShmEXPcc'
 CHANNEL_ID =  -1001699275031
 ADMIN_ID = 430692329
 async def approve_request (chat_join: ChatJoinRequest, bot: Bot):
-   msg= f"Ваша заявка одобрена!\n\nВступить в канал: https://t.me/+CK3azK4J6OkxN2Fi"
-   button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+CK3azK4J6OkxN2Fi', disable_web_page_preview=True)   
+   msg= f"Ваша заявка одобрена!\n\nВступить в канал: https://t.me/+oUNYkPX1fbxlZTZi"
+   button = InlineKeyboardButton(text='ВСТУПИТЬ', url='https://t.me/+oUNYkPX1fbxlZTZi', disable_web_page_preview=True)   
    markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
 
 
